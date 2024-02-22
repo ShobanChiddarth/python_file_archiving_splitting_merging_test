6c33dbe47eb1e3be7bae97e1996f0cd9a361feda
Shoban Chiddarth <messageshoban@gmail.com>
2024-02-22 19:07:12 +0530

    Added tests for splitting and merging
    Now `testing.py` does the following
    1. Create a 7z archive of the file
    2. Split it into parts of 2GB (or less)
    3. Merges the split parts into an archive file
    4. Extracts the archive

--- testing.py
+++ testing.py
@@ -1,9 +1,23 @@
 from main import *
 import os
 
-file_to_be_compressed = os.path.join( os.environ.get('HOME'), 'Downloads', 'ISO Files', 'debian-live-12.5.0-amd64-gnome.iso' )
+if not os.path.exists('./out'):
+    os.makedirs('./out')
+if not os.path.exists('./out/multipart'):
+    os.makedirs('./out/multipart')
+if not os.path.exists('./out/extraction'):
+    os.makedirs('./out/extraction')
+if not os.path.exists('./out/temp'):
+    os.makedirs('./out/temp')
+
+file_to_be_compressed = os.path.join( os.environ.get('HOME'), 'Downloads', 'ISO Files', 'kali-linux-2023.4-installer-everything-amd64.iso' )
 archive_to_be_created = './out/myarchive.7z'
+multipart_directory = './out/multipart/'
 extracting_directory = './out/extraction/'
+temp_archive = './out/temp/temp.7z'
 
 archive_single_file(file_to_be_compressed, archive_to_be_created)
-extract_archive(archive_to_be_created, extracting_directory)
+split_file(archive_to_be_created, multipart_directory)
+merge_parts(multipart_directory, temp_archive)
+extract_archive(temp_archive, extracting_directory)
+
